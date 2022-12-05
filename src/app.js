a655abf23c9351b4e319bd8c8e5555d805db8512
eblondel <emmanuel.blondel1@gmail.com>
2022-12-05 15:46:47 +0100

    testing patch for openlayers/ol-cesium #1012

--- src/app.js
+++ src/app.js
@@ -2,9 +2,25 @@ console.log('Hello world!');
 
 import 'ol/ol.css';
 import Map from 'ol/Map';
-import OLCesium from 'olcs/OLCesium.js';
+import OLCesium from 'ol-cesium/src/olcs/OLCesium.js';
 
-const ol2dMap = new Map({id: "map", target : "map"});
+import Map from 'ol/Map';
+import OSM from 'ol/source/OSM';
+import TileLayer from 'ol/layer/Tile';
+import View from 'ol/View';
+
+const ol2dMap = new Map({
+  layers: [
+    new TileLayer({
+      source: new OSM(),
+    }),
+  ],
+  target: 'map',
+  view: new View({
+    center: [0, 0],
+    zoom: 2,
+  }),
+});
 
 const ol3d = new OLCesium({map: ol2dMap}); // ol2dMap is the ol.Map instance
 ol3d.setEnabled(true);
